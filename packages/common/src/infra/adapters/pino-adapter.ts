067d4c12a3a3be67a97e393fe83fdd9c057481a9
Ricardo Barros <rcebarros@gmail.com>
2026-04-23 16:24:22 -0300

    feat(PinoAdapter): ensure LOG_LEVEL property is defined if level property is invalid

--- packages/common/src/infra/adapters/pino-adapter.ts
+++ packages/common/src/infra/adapters/pino-adapter.ts
@@ -1,15 +1,24 @@
 import type { Logger, LoggerOptions, TransportTargetOptions } from "pino";
 import { LoggerProtocol } from "../protocols/logger/logger-protocol.js";
 import pino from 'pino';
+import { CreateLoggerOptions } from "../types/logger/create-logger-options-type.js";
 
 export class PinoAdapter implements LoggerProtocol<Logger, LoggerOptions> {
 
     constructor() { }
 
-    createLogger(options: LoggerOptions): Logger {
+    createLogger(options: CreateLoggerOptions): Logger {
+        const { level, ...rest } = options
         const isDevelopmentEnv = process.env.NODE_ENV === 'development'
         const transport = isDevelopmentEnv ? this.getPrettyConfigs() : undefined;
-        return pino({ ...options, transport })
+        const validLevel = level ?? process.env.LOG_LEVEL
+
+        return pino({
+            ...options,
+            transport,
+            level: validLevel,
+            ...rest
+        })
     }
 
     private getPrettyConfigs(): TransportTargetOptions {
---
+++ packages/common/src/infra/types/logger/log-levels-type.ts
@@ -0,0 +1,6 @@
+export type logLevels =
+    | 'fatal'
+    | 'error'
+    | 'warning'
+    | 'info'
+    | 'debug'
