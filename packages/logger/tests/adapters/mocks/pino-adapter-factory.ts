7bccc6af1736b62b873ed3793a7a6843764e4d7f
Ricardo Barros <rcebarros@gmail.com>
2026-04-26 10:28:57 -0300

    test(PinoAdapter): ensure createFatalLog calls pino with valid transport property if NODE_ENV equals development

--- packages/logger/tests/adapters/mocks/pino-adapter-factory.ts
+++ packages/logger/tests/adapters/mocks/pino-adapter-factory.ts
@@ -1,16 +1,17 @@
-import { LoggerProtocol } from "@/src/protocols/logger-protocol.js";
 import { createRandomObject } from "@/tests/mocks/random-values.js";
 import { PinoAdapterMock } from "./pino-adapter-mock.js";
-import type { Logger, LoggerOptions } from "pino";
+import { PinoAdapter } from "@/src/adapters/pino-adapter.js";
+import { SystemWorkspaces } from "@ai-powered-backlog-classifier/shared";
+import { getRandomSystemaWorkspacesMock } from "@/tests/mocks/random-system-workspaces-mock.js";
 
 interface SutTypes {
     options: Record<string, any>
-    sut: LoggerProtocol<Logger, LoggerOptions>
+    sut: PinoAdapter
 }
 
-export const makePinoAdapter = (): SutTypes => {
+export const makePinoAdapter = (name: SystemWorkspaces = getRandomSystemaWorkspacesMock()): SutTypes => {
     const options = createRandomObject();
-    const sut = new PinoAdapterMock()
+    const sut = new PinoAdapterMock(name)
     return {
         sut,
         options
--- packages/logger/tests/adapters/pino-adapter.spec.ts
+++ packages/logger/tests/adapters/pino-adapter.spec.ts
@@ -1,17 +1,20 @@
-import { faker } from '@faker-js/faker';
 import { describe, it, jest, expect, afterEach, beforeEach } from '@jest/globals'
+import { getRandomSystemaWorkspacesMock } from '../mocks/random-system-workspaces-mock.js'
+import { logLevelsOptions, SystemWorkspaces } from '@ai-powered-backlog-classifier/shared'
 
 // Mock Pino
 jest.unstable_mockModule('pino', () => ({ default: jest.fn() }))
 const { default: pino } = await import('pino')
 const { makePinoAdapter } = await import('./mocks/pino-adapter-factory.js')
 
-describe('LoggerAdapter Suite', () => {
+describe('PinoAdapter Suite', () => {
 
+    let systemWorkspace: SystemWorkspaces
     let originalEnv: NodeJS.ProcessEnv
 
     beforeEach(() => {
         originalEnv = process.env
+        systemWorkspace = getRandomSystemaWorkspacesMock()
     })
 
     afterEach(() => {
@@ -19,19 +22,25 @@ describe('LoggerAdapter Suite', () => {
         process.env = originalEnv
     })
 
-    describe('PinoAdapter', () => {
+    describe('CreateFatalLog', () => {
 
-        it('Should confirm if logger parameter was called with correct arguments', () => {
-            const { options, sut } = makePinoAdapter()
-            sut.createLogger(options)
+        it('Should calls pino with with correct values', () => {
+            const { options, sut } = makePinoAdapter(systemWorkspace)
+            sut.createFatalLog(options)
             expect(pino).toHaveBeenCalledTimes(1)
-            expect(pino).toHaveBeenCalledWith(options)
+            expect(pino).toHaveBeenCalledWith(
+                {
+                    ...options,
+                    name: systemWorkspace,
+                    level: logLevelsOptions.fatal
+                }
+            )
         })
 
         it('Should calls pino with valid transport property if NODE_ENV equals development', () => {
             process.env.NODE_ENV = 'development'
             const { options, sut } = makePinoAdapter()
-            sut.createLogger(options)
+            sut.createFatalLog(options)
             expect(pino).toHaveBeenCalledWith(
                 expect.objectContaining({
                     transport: {
@@ -45,17 +54,6 @@ describe('LoggerAdapter Suite', () => {
             )
         })
 
-        it('Should calls pino with an undefined transport property value if NODE_ENV is not equals development', () => {
-            process.env.NODE_ENV = process.env.NODE_ENV + faker.word.noun() // to force non-existent NODE_ENV value
-            const { options, sut } = makePinoAdapter()
-            sut.createLogger(options)
-            expect(pino).toHaveBeenCalledWith(
-                expect.objectContaining({ transport: undefined })
-            )
-        })
-
-
     })
-
 
 })
