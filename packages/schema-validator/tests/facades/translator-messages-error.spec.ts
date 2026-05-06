f6991aac7460823575bc73362cd214b5186fcb82
Ricardo Barros <rcebarros@gmail.com>
2026-05-06 11:02:10 -0300

    test(TranslatorMessagesError): ensure calls init method with correct resources property object

--- packages/schema-validator/tests/facades/translator-messages-error.spec.ts
+++ packages/schema-validator/tests/facades/translator-messages-error.spec.ts
@@ -1,5 +1,7 @@
+import { languages } from '@/src/index.js'
 import { languageOptions } from '@/src/enums/language-options.js'
-import { describe, it, expect, jest } from '@jest/globals'
+import { describe, it, expect, jest, beforeEach } from '@jest/globals'
+import translation from 'zod-i18n-map/locales/pt/zod.json' with { type: "json" };
 
 jest.unstable_mockModule('i18next', () => ({ default: { init: jest.fn() } }))
 const { default: i18next } = await import('i18next')
@@ -7,8 +9,14 @@ const { makeTranslatorMessagesErro } = await import('./mocks/translator-messages
 
 describe('TranslatorMessagesError Suite', () => {
 
+    let language: languages
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+        language = languageOptions.portugues
+    })
+
     it('Should calls init method with correct lng property value', async () => {
-        const language = languageOptions.portugues
         const { sut } = makeTranslatorMessagesErro(language)
         await sut.configLanguage()
         expect(i18next.init).toHaveBeenCalledTimes(1)
@@ -19,7 +27,20 @@ describe('TranslatorMessagesError Suite', () => {
         )
     })
 
-    it.todo('Should calls init method with correct resources property object')
+    it('Should calls init method with correct resources property object', async () => {
+        language = languageOptions.ingles
+        const { sut } = makeTranslatorMessagesErro(language)
+        await sut.configLanguage()
+        expect(i18next.init).toHaveBeenCalledTimes(1)
+        expect(i18next.init).toHaveBeenCalledWith(
+            expect.objectContaining({
+                resources: {
+                    [language]: { zod: translation }
+                }
+            })
+        )
+    })
+
     it.todo('Should calls setErrorMap with correct global class')
 
 })
