f186506aff985665c32a123593f94e7f66e4db4d
Ricardo Barros <rcebarros@gmail.com>
2026-05-06 13:06:01 -0300

    test(TranslatorMessagesError): ensure calls zod config method with correct language value

--- packages/schema-validator/tests/facades/translator-messages-error.spec.ts
+++ packages/schema-validator/tests/facades/translator-messages-error.spec.ts
@@ -1,47 +1,36 @@
 import { languages } from '@/src/index.js'
 import { languageOptions } from '@/src/enums/language-options.js'
 import { describe, it, expect, jest, beforeEach } from '@jest/globals'
-import translation from 'zod-i18n-map/locales/pt/zod.json' with { type: "json" };
+import { faker } from '@faker-js/faker'
 
-jest.unstable_mockModule('i18next', () => ({ default: { init: jest.fn() } }))
-const { default: i18next } = await import('i18next')
+const languageSpy = jest.fn()
+const language: languages = faker.helpers.arrayElement([languageOptions.ingles, languageOptions.portugues])
+
+jest.unstable_mockModule('zod', () => ({
+    __esModule: true,
+    ...jest.requireActual('zod') as object,
+    config: jest.fn(),
+    locales: {
+        [language]: languageSpy
+    }
+}))
+
 const { makeTranslatorMessagesErro } = await import('./mocks/translator-messages-error-factory.js')
+const z = await import('zod')
+const zMocked = z as jest.Mocked<typeof z>
 
 describe('TranslatorMessagesError Suite', () => {
 
-    let language: languages
-
     beforeEach(() => {
         jest.clearAllMocks()
-        language = languageOptions.portugues
     })
 
-    it('Should calls init method with correct lng property value', async () => {
+    it('Should calls zod config method with correct language value', async () => {
         const { sut } = makeTranslatorMessagesErro(language)
         await sut.configLanguage()
-        expect(i18next.init).toHaveBeenCalledTimes(1)
-        expect(i18next.init).toHaveBeenCalledWith(
-            expect.objectContaining({
-                lng: language
-            })
-        )
+        expect(z.config).toHaveBeenCalledTimes(1)
+        expect(zMocked.config.mock.calls[0][0]).toEqual(languageSpy())
     })
-
-    it('Should calls init method with correct resources property object', async () => {
-        language = languageOptions.ingles
-        const { sut } = makeTranslatorMessagesErro(language)
-        await sut.configLanguage()
-        expect(i18next.init).toHaveBeenCalledTimes(1)
-        expect(i18next.init).toHaveBeenCalledWith(
-            expect.objectContaining({
-                resources: {
-                    [language]: { zod: translation }
-                }
-            })
-        )
-    })
-
-    it.todo('Should calls setErrorMap with correct global class')
 
 })
 
