ea43883d20dae5200ded9fee2f8accef807b97fa
Ricardo Barros <rcebarros@gmail.com>
2026-05-06 13:05:50 -0300

    feat(TranslatorMessagesError): ensure calls zod config method with correct language value

--- packages/schema-validator/src/facades/translator-messages-error.ts
+++ packages/schema-validator/src/facades/translator-messages-error.ts
@@ -1,17 +1,11 @@
-import i18next from "i18next";
 import { languages } from "../types/languages.js";
 import { ConfigureLanguageProtocol } from "../protocols/configure-language-protocol.js";
-import translation from 'zod-i18n-map/locales/pt/zod.json' with { type: "json" };
+import * as z from 'zod'
 
 export class TranslatorMessagesError implements ConfigureLanguageProtocol {
     constructor(private readonly language: languages) { }
 
     async configLanguage(): Promise<void> {
-        await i18next.init({
-            lng: this.language,
-            resources: {
-                [this.language]: { zod: translation }
-            }
-        })
+        z.config(z.locales[this.language]())
     }
 }
