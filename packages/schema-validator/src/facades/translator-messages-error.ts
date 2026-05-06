b80bc30cc0c91f23b14b0cdd887f3bc5acdbd791
Ricardo Barros <rcebarros@gmail.com>
2026-05-06 11:01:41 -0300

    feat(TranslatorMessagesError): ensure calls init method with correct resources property object

--- packages/schema-validator/src/facades/translator-messages-error.ts
+++ packages/schema-validator/src/facades/translator-messages-error.ts
@@ -1,13 +1,17 @@
 import i18next from "i18next";
 import { languages } from "../types/languages.js";
 import { ConfigureLanguageProtocol } from "../protocols/configure-language-protocol.js";
+import translation from 'zod-i18n-map/locales/pt/zod.json' with { type: "json" };
 
 export class TranslatorMessagesError implements ConfigureLanguageProtocol {
     constructor(private readonly language: languages) { }
 
     async configLanguage(): Promise<void> {
         await i18next.init({
-            lng: this.language
+            lng: this.language,
+            resources: {
+                [this.language]: { zod: translation }
+            }
         })
     }
 }
